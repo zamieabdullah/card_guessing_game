0df05cd48b1a42189e928e42513b7d3c7c972380
zamieabdullah <zamieabdullah0@gmail.com>
2021-11-28 16:11:46 -0500

    complete implementation of CardStack

--- CardStack.py
+++ CardStack.py
@@ -26,13 +26,28 @@ class CardStack:
 
         self.length+=1
 
+    def removeFront(self):
+        if self.node != None:
+            if self.getLength() == 1:
+                self.node = None
+            else:
+                temp = self.node
+                self.node = temp.next
+            
+            self.length-=1
+                
+
     def print_stack(self):
         print("Stack length:", self.getLength())
         temp = self.node
         print("Cards on Stack")
-        while temp != None:
-            print(temp.card.getValue());
-            temp = temp.next
+        
+        if self.getLength() != 0:
+            while temp != None:
+                print(temp.card.getValue());
+                temp = temp.next
+        else:
+            print("No cards on stack!");
 
 
 # Testing
@@ -40,6 +55,19 @@ if __name__ == "__main__":
     deck = CardStack()
 
     deck.pushBack(Card(4))
+    deck.print_stack()
+    
     deck.pushBack(Card(5))
+    deck.print_stack()
+    
     deck.pushBack(Card(6))
+    deck.print_stack()
+    
+    deck.removeFront()
+    deck.print_stack()
+    
+    deck.removeFront()
+    deck.print_stack()
+    
+    deck.removeFront()
     deck.print_stack()
--- Game.py
+++ Game.py
@@ -1,1 +1,2 @@
 from card import Card
+from CardStack import CardStack
